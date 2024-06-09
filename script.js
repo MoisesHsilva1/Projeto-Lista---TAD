8fa72fdb349c36efc21d6af2e853a330a3b372b8
Moises Henrique <moihgm67@gmail.com>
2024-06-08 23:04:29 -0300

    Atualizando Projeto

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const ItemInput =  document.getElementById('ItemInput');
-const ButonFiliar =  document.getElementById('FiliarButon');
+const ButonFiliar =  document.getElementById('FiliarButton');
 const ButtonTirarFila = document.getElementById('Desenfilirear');
 const ElementoFila = document.getElementById('FilaLista');
 
-function validarInput () {
-    if(ItemInput.value == '') {
-        window.alert('Digite um valor correto');
-        return false;
-    } else {
-        return true;
-    }
 
+function validarInput () {
+    window.alert("teste")
+   /* if(ItemInput.value == '') {
+        window.alert('Digite um valor correto');
+       // return false;
+    } else {
+      //  return true;
+    }
+    */
 }
 
 ButonFiliar.addEventListener('click', function(){
-    validarInput()
+     validarInput();
 })
